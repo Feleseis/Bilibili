3978d4db0d6a4ebea01420bf681c05c74305bc69
Feleseis <124481147@qq.com>
2017-03-15 20:02:45 +0800

    3.15

---
+++ js/ajax.js
@@ -0,0 +1,13 @@
+// (function() {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'http://space.bilibili.com/ajax/member/MyInfo', true);
+//     // xhr.setRequestHeader('header','value');
+//     xhr.send(null);
+//     xhr.onreadystatechange = function() {
+//         if (xhr.readyState === 4 && xhr.status === 200) {
+//             //...
+//             const data = JSON.parse(xhr.responseText);
+//             console.log(data);
+//         }
+//     };
+// })();
---
+++ js/data/videoInfo.js
@@ -0,0 +1,1 @@
+const videoInfo = {"code":0,"data":{"view":111593,"danmaku":608,"reply":523,"favorite":593,"coin":2732,"share":127,"now_rank":0,"his_rank":0},"message":""};
